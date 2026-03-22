bdde2eaccbdf9e563519d90f87b1b4cf360ca308
Lucas Manoel <lucasmlunag@gmail.com>
2026-03-22 16:47:54 -0300

    segundo commit

---
+++ eventos.js
@@ -0,0 +1,13 @@
+var abrir = false;
+
+function mude() {
+    const box = document.getElementById("box");
+    
+    abrir = !abrir;
+
+    if (abrir === true) {
+        box.style.display = "block";
+    } else {
+        box.style.display = "none";
+    }
+}
