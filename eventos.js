38aec13680dcfd83569ea1bcd738f6afe5322d6f
Lucas Manoel <lucasmlunag@gmail.com>
2026-03-24 14:21:42 -0300

    2 commit

--- eventos.js
+++ eventos.js
@@ -2,12 +2,10 @@ var abrir = false;
 
 function mude() {
     const box = document.getElementById("box");
-    
-    abrir = !abrir;
-
-    if (abrir === true) {
-        box.style.display = "block";
-    } else {
-        box.style.display = "none";
-    }
+        abrir = !abrir;
+        if (abrir === true) {
+            box.style.display = "block";
+        } else {
+            box.style.display = "none";
+        }
 }
